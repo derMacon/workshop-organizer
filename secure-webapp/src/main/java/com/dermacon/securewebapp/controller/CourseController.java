92ae6d3230680d3804eed22eed37eda6ffe24166
derMacon <silash169@gmail.com>
2020-10-27 05:20:13 +0100

    added specific course endpoint

--- secure-webapp/src/main/java/com/dermacon/securewebapp/controller/CourseController.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/CourseController.java
@@ -1,10 +1,12 @@
 package com.dermacon.securewebapp.controller;
 
+import com.dermacon.securewebapp.data.Course;
 import com.dermacon.securewebapp.data.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CourseController {
@@ -19,6 +21,18 @@ public class CourseController {
         model.addAttribute("allCourses", courseRepository.findAll());
 
         return "courses";
+    }
+
+    @RequestMapping("/courses/specific")
+    public String courses(Model model, @RequestParam String id) {
+
+        System.out.println("with id: " + id);
+
+        Long id_long = Long.parseLong(id);
+        Course course = courseRepository.findByCourseId(id_long);
+        model.addAttribute("currCourse", course);
+
+        return "specificCourse";
     }
 
 
--- secure-webapp/src/main/java/com/dermacon/securewebapp/data/CourseRepository.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/data/CourseRepository.java
@@ -3,4 +3,5 @@ package com.dermacon.securewebapp.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
+    Course findByCourseId(Long id);
 }
