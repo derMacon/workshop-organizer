5a55fa776c07733016a0f1a1cc90adc1b22b3525
derMacon <silash169@gmail.com>
2020-12-18 23:31:24 +0100

    clean up

---
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/controller/CourseOverviewController.java
@@ -0,0 +1,15 @@
+package com.dermacon.securewebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("courses")
+public class CourseOverviewController {
+
+//    @RequestMapping(path = "/")
+//    public String showOverview
+
+
+
+}
