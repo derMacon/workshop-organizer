0b622dfda5db350d2ae8342bff7e356e275fa8a1
derMacon <silash169@gmail.com>
2020-12-31 00:15:02 +0100

    resolved mail error config

--- secure-webapp/src/main/java/com/dermacon/securewebapp/service/MailService.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/service/MailService.java
@@ -26,123 +26,123 @@ import java.util.Set;
 @Service
 public class MailService {
 
-//    /*
-//     * The Spring Framework provides an easy abstraction for sending email by using
-//     * the JavaMailSender interface, and Spring Boot provides auto-configuration for
-//     * it as well as a starter module.
-//     */
-//    private JavaMailSender javaMailSender;
-//
-//    /**
-//     *
-//     * @param javaMailSender
-//     */
-//    public MailService(JavaMailSender javaMailSender) {
-//        this.javaMailSender = javaMailSender;
-//    }
-//
-//    /**
-//     * Send greeting mail to person who entered a course
-//     * @param person person new to the course
-//     * @param course course to which a the user subscribed
-//     */
-//    public void sendGreeting(Person person, Course course) {
-//        SimpleMailMessage mail = new SimpleMailMessage();
-//        mail.setTo(person.getEmail());
-//        mail.setSubject("Welcome to " + course.getCourseName() + " workshop");
-//        mail.setText(course.toString());
-//
-//        LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
-//        // todo uncomment
-////        javaMailSender.send(mail);
-//    }
-//
-//    /**
-//     * Sends a mail to confirm the user he unsubscribed from the course
-//     * @param person person to which the mail will be send
-//     * @param course course he left
-//     */
-//    public void sendDropoutConfirmation(Person person, Course course) {
-//        SimpleMailMessage mail = new SimpleMailMessage();
-//        mail.setTo(person.getEmail());
-//        mail.setSubject("Confirmation for checkout");
-//        mail.setText("leaving course:\n" + course.toString());
-//
-//        LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
-//        // todo uncomment
-////        javaMailSender.send(mail);
-//    }
-//
-//    /**
-//     * Send announcement to a given set of persons
-//     * @param persons set of people
-//     * @param announcement announcement to send per mail
-//     */
-//    public void sendAnnouncement(Set<Person> persons, Announcement announcement) {
-//        for (Person person : persons) {
-//            SimpleMailMessage mail = new SimpleMailMessage();
-//            mail.setTo(person.getEmail());
-//            mail.setSubject("Announcement: " + announcement.getTitle());
-//            mail.setText(announcement.getContent());
-//
-//            LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
-//            // todo uncomment
-////        javaMailSender.send(mail);
-//        }
-//    }
-//
-//
-//    /**
-//     *
-//     * @throws MailException
-//     */
-//
-//    public void sendEmail(Person person) throws MailException {
-//
-//        /*
-//         * This JavaMailSender Interface is used to send Mail in Spring Boot. This
-//         * JavaMailSender extends the MailSender Interface which contains send()
-//         * function. SimpleMailMessage Object is required because send() function uses
-//         * object of SimpleMailMessage as a Parameter
-//         */
-//
-//        LoggerSingleton.getInstance().info("new mail: " + person);
-//
-//        SimpleMailMessage mail = new SimpleMailMessage();
-//
-//        mail.setTo("rwl93183@cuoly.com");
-////        mail.setTo(person.getEmail());
-//        mail.setSubject("Testing Mail API");
-//        mail.setText("Hurray ! You have done that dude...");
-//
-//        /*
-//         * This send() contains an Object of SimpleMailMessage as an Parameter
-//         */
+    /*
+     * The Spring Framework provides an easy abstraction for sending email by using
+     * the JavaMailSender interface, and Spring Boot provides auto-configuration for
+     * it as well as a starter module.
+     */
+    private JavaMailSender javaMailSender;
+
+    /**
+     *
+     * @param javaMailSender
+     */
+    public MailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    /**
+     * Send greeting mail to person who entered a course
+     * @param person person new to the course
+     * @param course course to which a the user subscribed
+     */
+    public void sendGreeting(Person person, Course course) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(person.getEmail());
+        mail.setSubject("Welcome to " + course.getCourseName() + " workshop");
+        mail.setText(course.toString());
+
+        LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
+        // todo uncomment
 //        javaMailSender.send(mail);
-//    }
-//
-//    /**
-//     * This function is used to send mail that contains a attachment.
-//     *
-//     * @param user
-//     * @throws MailException
-//     * @throws MessagingException
-//     */
-//    public void sendEmailWithAttachment(User user) throws MailException, MessagingException {
-//
-//        MimeMessage message = javaMailSender.createMimeMessage();
-//
-//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//
-////        helper.setTo(user.getEmailAddress());
-//        helper.setSubject("Testing Mail API with Attachment");
-//        helper.setText("Please find the attached document below.");
-//
-//
-//        ClassPathResource classPathResource = new ClassPathResource("Attachment.pdf");
-//        helper.addAttachment(classPathResource.getFilename(), classPathResource);
-//
-//        javaMailSender.send(message);
-//    }
+    }
+
+    /**
+     * Sends a mail to confirm the user he unsubscribed from the course
+     * @param person person to which the mail will be send
+     * @param course course he left
+     */
+    public void sendDropoutConfirmation(Person person, Course course) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(person.getEmail());
+        mail.setSubject("Confirmation for checkout");
+        mail.setText("leaving course:\n" + course.toString());
+
+        LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
+        // todo uncomment
+//        javaMailSender.send(mail);
+    }
+
+    /**
+     * Send announcement to a given set of persons
+     * @param persons set of people
+     * @param announcement announcement to send per mail
+     */
+    public void sendAnnouncement(Set<Person> persons, Announcement announcement) {
+        for (Person person : persons) {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(person.getEmail());
+            mail.setSubject("Announcement: " + announcement.getTitle());
+            mail.setText(announcement.getContent());
+
+            LoggerSingleton.getInstance().info("sending mail: " + mail.toString());
+            // todo uncomment
+//        javaMailSender.send(mail);
+        }
+    }
+
+
+    /**
+     *
+     * @throws MailException
+     */
+
+    public void sendEmail(Person person) throws MailException {
+
+        /*
+         * This JavaMailSender Interface is used to send Mail in Spring Boot. This
+         * JavaMailSender extends the MailSender Interface which contains send()
+         * function. SimpleMailMessage Object is required because send() function uses
+         * object of SimpleMailMessage as a Parameter
+         */
+
+        LoggerSingleton.getInstance().info("new mail: " + person);
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setTo("rwl93183@cuoly.com");
+//        mail.setTo(person.getEmail());
+        mail.setSubject("Testing Mail API");
+        mail.setText("Hurray ! You have done that dude...");
+
+        /*
+         * This send() contains an Object of SimpleMailMessage as an Parameter
+         */
+        javaMailSender.send(mail);
+    }
+
+    /**
+     * This function is used to send mail that contains a attachment.
+     *
+     * @param user
+     * @throws MailException
+     * @throws MessagingException
+     */
+    public void sendEmailWithAttachment(User user) throws MailException, MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+//        helper.setTo(user.getEmailAddress());
+        helper.setSubject("Testing Mail API with Attachment");
+        helper.setText("Please find the attached document below.");
+
+
+        ClassPathResource classPathResource = new ClassPathResource("Attachment.pdf");
+        helper.addAttachment(classPathResource.getFilename(), classPathResource);
+
+        javaMailSender.send(message);
+    }
 
 }
