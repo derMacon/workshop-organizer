c080cc2c5e574196c43f11c132cb78eb65809499
derMacon <silash169@gmail.com>
2021-01-02 21:42:15 +0100

    added log4j config

--- secure-webapp/src/main/java/com/dermacon/securewebapp/SecureWebappApplication.java
+++ secure-webapp/src/main/java/com/dermacon/securewebapp/SecureWebappApplication.java
@@ -3,11 +3,23 @@ package com.dermacon.securewebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.log4j.Logger;
+
 @SpringBootApplication
 public class SecureWebappApplication {
 
+
+    static Logger log = Logger.getLogger(SecureWebappApplication.class.getName());
+
     public static void main(String[] args) {
-        SpringApplication.run(SecureWebappApplication.class, args);
+//        SpringApplication.run(SecureWebappApplication.class, args);
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
+
     }
 
 }
