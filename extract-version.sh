616301145c6ec0ccf521608b82dfb7782e24a8a5
derMacon <silash169@gmail.com>
2021-01-18 01:25:45 +0100

    tried implementing semantic versioning

---
+++ extract-version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+#to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
+if [ -z "$NEEDS_TAG" ]; then
+
+	#increment only if needed
+	mvn --batch-mode release:update-versions versions:set -DremoveSnapshot
+
+	MVN_VERSION=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+
+	git tag $MVN_VERSION
+	git push --tags
+	git push
+else
+  echo "Already a tag on this commit"
+fi
+
+exit 0
